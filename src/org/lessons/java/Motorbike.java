f749c34650774cfb8ce88af675bde1a98e32bc21
CampanelliEmanuele <emanuele.campanelli.work@gmail.com>
2023-12-14 17:03:01 +0100

    Update: all classes

--- src/org/lessons/java/Motorbike.java
+++ src/org/lessons/java/Motorbike.java
@@ -3,4 +3,17 @@ package org.lessons.java;
 public class Motorbike extends Vehicle {
     private boolean hasEasel;
 
+    public Motorbike(String plate, int registrationYear, boolean hasEasel) throws IllegalArgumentException {
+        super(plate, registrationYear);
+        this.hasEasel = hasEasel;
+    }
+
+    @Override
+    public String toString() {
+        return "Motorbike{" +
+                "hasEasel=" + hasEasel +
+                ", plate='" + plate + '\'' +
+                ", registrationYear=" + registrationYear +
+                '}';
+    }
 }
